de2a43b7acb58b153e9cb08bcc07895e708b69de
JCarroll2019 <jmcarroll2018@gmail.com>
2023-04-17 15:26:30 -0500

    initial commit

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 import './App.css';
 
-function App() {
+const  App = () => {
   return (
     <div className="App">
       <header className="App-header">
